e1e854070051d084dc8ce445f72e5e1a041eef6f
isa03 <iva.savic@brueggli.ch>
2024-10-16 15:39:41 +0200

    feat: get request

--- Program.py
+++ Program.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import urllib.parse
+import urllib.request
+
+url = "http://transport.opendata.ch/v1/stationboard"
+params = {
+    "station": "Zurich",
+    "limit": 3,
+    "fields[]": "stationboard/stop/departure",
+    "fields[]": "stationboard/stop/delay",
+    "fields[]": "stationboard/category",
+    "fields[]": "stationboard/number",
+    "fields[]": "stationboard/to"
+}
+
+query_string = urllib.parse.urlencode(params)
+
+url = url + "?" + query_string
+
+with urllib.request.urlopen(url) as response:
+    response_text = response.read()
+    print(response_text)
